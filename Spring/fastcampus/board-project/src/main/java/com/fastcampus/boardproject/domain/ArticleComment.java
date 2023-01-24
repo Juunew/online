763ef9f548c22af7d137ecc0ef5f25032354b579
hyukjun <hyukjuun@gmail.com>
2023-01-24 23:42:22 +0900

    #2 - domain(Article, ArticleComment) @EntityListeners 추가

--- Spring/fastcampus/board-project/src/main/java/com/fastcampus/boardproject/domain/ArticleComment.java
+++ Spring/fastcampus/board-project/src/main/java/com/fastcampus/boardproject/domain/ArticleComment.java
@@ -7,18 +7,20 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Getter
 @ToString
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
-@Getter
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class ArticleComment {
 
