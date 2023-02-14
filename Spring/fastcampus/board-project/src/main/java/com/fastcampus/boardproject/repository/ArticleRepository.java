1afe42cc7cf0b28b52ca622ea3387aab13c28c8a
hyukjun <hyukjuun@gmail.com>
2023-02-14 18:30:34 +0900

    #7 - fastCampus board-project releases v1.0

--- Spring/fastcampus/board-project/src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
+++ Spring/fastcampus/board-project/src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.fastcampus.boardproject.repository;
 
 import com.fastcampus.boardproject.domain.Article;
 import com.fastcampus.boardproject.domain.QArticle;
+import com.fastcampus.boardproject.repository.querydsl.ArticleRepositoryCustom;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.domain.Page;
@@ -15,7 +16,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
-        // 검색기능 추가
+        ArticleRepositoryCustom,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
 
@@ -25,11 +26,14 @@ public interface ArticleRepository extends
     Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
     Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
+    void deleteByIdAndUserAccount_UserId(Long articleId, String userid);
+
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true);
         bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
         bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
         bindings.bind(root.createdAt).first(DateTimeExpression::eq);
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
