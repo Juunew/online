33b33afeb84da4258a83d2a1c52aa844373c2629
hyukjun <hyukjuun@gmail.com>
2023-03-07 00:15:38 +0900

    #1 - Member, Team domain

---
+++ Spring/inflearn/data-jpa/src/main/java/com/study/datajpa/entity/Team.java
@@ -0,0 +1,29 @@
+package com.study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@ToString(of = {"id", "name"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Team {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
